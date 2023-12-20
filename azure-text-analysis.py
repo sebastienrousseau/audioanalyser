e5370967fe9c269c2c0d807f018102ac79f7dd88
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-12-20 13:20:05 +0000

    fix(azure-audio-text): :bug: Enhanced results
    Enhanced Summary in save_results Function:  Detailed Sentiment Analysis: Added sentiment scores for positive, neutral, and negative sentiments. Expanded Key Entities Summary: Increased the number of key entities displayed to five. More In-depth Topic Analysis: Included additional key phrases, displaying up to five key phrases for a more detailed summary. Corrected Scope Issue:  Resolved the NameError issue by correctly accessing key_phrases_result and other results from the results dictionary within the save_results function. Removed Redundant Code:  Eliminated the redundancy in saving results. The script now saves the analysis results in text, JSON, and SQLite formats only once, streamlining the process. Code Refactoring:  Refactored portions of the script for improved readability and maintainability, particularly in the save_results function.

--- azure-text-analysis.py
+++ azure-text-analysis.py
@@ -82,24 +82,28 @@ async def save_results(filename, results):
     with open(txt_filename, 'w') as file:
         current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         # Executive summary format
-        sentiment_result = results['sentiment'][0]
         file.write('Transcription analysis\n\n')
         file.write(f'File: {filename}\n')
         file.write(f'Time: {current_time}\n\n')
         file.write(f'Summary:\n')
 
         # Sentiment Analysis Summary
+        sentiment_result = results['sentiment'][0]
         file.write(f'   • Overall Sentiment: {sentiment_result.sentiment}.\n')
+
+        # Enhanced Sentiment Analysis Summary
+        sentiment_scores = sentiment_result.confidence_scores
+        file.write(f'   • Sentiment Scores - Positive: {sentiment_scores.positive:.2f}, Neutral: {sentiment_scores.neutral:.2f}, Negative: {sentiment_scores.negative:.2f}\n')
 
         # Key Entities Summary
         entities_result = results['entities'][0]
         if entities_result.entities:
-            file.write('   • Key Entities Identified: ' + ', '.join([entity.text for entity in entities_result.entities[:3]]) + '.\n')
+            file.write('   • Key Entities Identified: ' + ', '.join([entity.text for entity in entities_result.entities[:5]]) + '.\n')
 
-        # Key Phrases Summary
+        # Enhanced Key Phrases Summary
         key_phrases_result = results['key_phrases'][0]
         if key_phrases_result.key_phrases:
-            file.write('   • Notable Topics: ' + ', '.join(key_phrases_result.key_phrases[:3]) + '.\n')
+            file.write('   • Notable Topics: ' + ', '.join(key_phrases_result.key_phrases[:5]) + '.\n')
 
         # Language Detection Summary
         language_result = results['language'][0]
@@ -131,30 +135,6 @@ async def save_results(filename, results):
         conn.commit()
     print(f'Saved analysis of {filename} to database.')
 
-    serializable_results = convert_to_serializable(results)
-
-    # Save to TXT
-    txt_filename = f"{filename}_analysis.txt"
-    with open(txt_filename, 'w') as file:
-        file.write(json.dumps(serializable_results, indent=4, ensure_ascii=False))
-    print(f"Saving results to {txt_filename}")
-
-    # Save to JSON
-    json_filename = f"{filename}_analysis.json"
-    with open(json_filename, 'w') as json_file:
-        json.dump(serializable_results, json_file, indent=4)
-    print(f"Saving results to {json_filename}")
-
-    # Save to SQLite
-    db_filename = 'text_analysis.db'
-    with sqlite3.connect(db_filename) as conn:
-        cursor = conn.cursor()
-        cursor.execute(f'''CREATE TABLE IF NOT EXISTS {DB_TABLE_NAME}
-                          (filename TEXT, analysis TEXT)''')
-        cursor.execute(f"INSERT INTO {DB_TABLE_NAME} (filename, analysis) VALUES (?, ?)",
-                       (filename, json.dumps(serializable_results)))
-        conn.commit()
-    print(f"Saved analysis of {filename} to database.")
 
 async def main():
     endpoint = AZURE_LANGUAGE_ENDPOINT
